5478fdf2d6db3202281d1651c8db20bb234d7e72
Mikdad <mikdad.kanbar1@gmail.com>
2022-12-02 14:47:01 +0100

    adding 10 functions

---
+++ connect.py
@@ -0,0 +1,35 @@
+
+import pandas as pd
+import psycopg2
+from   config import config  
+
+def sql(command) :
+   
+    conn = None
+    try:
+        
+        params = config()
+        
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+        conn.autocommit = True
+		
+        cur = conn.cursor()
+        
+        cur.execute(command)
+        
+        result = pd.DataFrame (cur.fetchall())
+         
+#         for r in result:
+#             print (r)
+        return result
+       
+	 
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
---
+++ main.py
@@ -0,0 +1,167 @@
+from connect import *
+
+
+
+def user_exist (username) :
+    users=  sql('select distinct username  from users')
+    username= (username,)
+    
+    if username in users:
+        return True
+    return False
+    
+    
+    
+    
+    
+    
+    
+def sign_up(username):
+    if not user_exist(username) :
+        cmd=f"insert into users (username) values ('{username}') "
+        sql(cmd)
+        print('added!')
+    else :
+        print ('user already exists')
+
+
+        
+def search_by_author(author)   : 
+    return sql(f"SELECT * FROM books where author='{author}'")
+    
+
+def recently_added(*genre):
+    if len(genre) ==0 : 
+        return sql (f"SELECT * FROM books limit 5")
+    else:
+        genre=list(genre)[0]
+        return sql(f"select * from books where genre='{genre}' limit 5 ")
+    
+def most_read_books(*genre) :
+    if len(genre) ==0 : 
+            return sql (f"""
+
+                select x.count , books.* from 
+                (select b.book_id  , sum (a.read) as count from
+                 (select * from books) b
+                 inner join 
+                (select book_id,read from users where read is not null) a ON a.book_id=b.book_id
+                GROUP BY b.book_id ) x inner join books  ON x.book_id=books.book_id limit 10
+
+                    
+                    """)
+    else:
+        genre=list(genre)[0]
+        return sql(f"""
+                  
+
+            select x.count , books.* from 
+            (
+            select b.book_id  , sum (a.read) as count from
+             (select * from books where genre='{genre}' ) b
+             inner join 
+            (select book_id,read from users where read is not null) a ON a.book_id=b.book_id
+            GROUP BY b.book_id ) x inner join books  ON x.book_id=books.book_id limit 10
+        """)
+def  most_favorite_books (*genre) :
+    if len(genre) ==0 : 
+            return sql (f"""
+
+                select x.count , books.* from 
+                (select b.book_id  , sum (a.fav) as count from
+                 (select * from books) b
+                 inner join 
+                (select book_id,fav from users where fav is not null) a ON a.book_id=b.book_id
+                GROUP BY b.book_id ) x inner join books  ON x.book_id=books.book_id limit 10
+
+                    
+                    """)
+    else:
+        genre=list(genre)[0]
+        return sql(f"""
+                  
+
+            select x.count , books.* from 
+            (
+            select b.book_id  , sum (a.fav) as count from
+             (select * from books where genre='{genre}' ) b
+             inner join 
+            (select book_id,fav from users where fav is not null) a ON a.book_id=b.book_id
+            GROUP BY b.book_id ) x inner join books  ON x.book_id=books.book_id limit 10
+        """)
+    
+    
+    
+def most_read_genres   ():
+    return sql ("""
+
+    select g.genre, sum (u.read) as count FROM 
+     ( select genre, book_id from books ) g 
+     inner join 
+     (select read, book_id from users  where read is not null) u ON g.book_id=u.book_id
+    GROUP BY genre ORDER BY count DESC LIMIT 5
+    """)
+def most_read_authors () :
+     return sql ("""
+
+    select g.author, sum (u.read) as count FROM 
+     ( select author, book_id from books ) g 
+     inner join 
+     (select read, book_id from users  where read is not null) u ON g.book_id=u.book_id
+    GROUP BY author ORDER BY count DESC LIMIT 5
+    """)
+
+def borrow_book (book_id, username) : 
+    pass
+#         -control user (make sure it exists or not , if not > msg)
+#         -if book avalable : 
+#             add a row to users , with username + borrowed =1
+#             change the quzntity of the book by -1
+    
+def return_book (book_id, username) : 
+    pass
+#     after controling user : 
+#         search for book_id + username in users 
+#         if borowed is 1 > change it to null ? ( or delete row ? )
+#         change the quzntity of the book by +1
+    
+def mark_read (book_id, username)  :
+    pass
+#     control user 
+#     add a row to users : with book id + username + read=1
+    
+def mark_will_read   (book_id, username) :
+    pass
+#     same logic for the previous, make will_read=1
+    
+def fav_book (  username) :
+    pass
+#      same logic for the previous, make fav=1
+    
+def my_books (username)    :
+    pass
+#     in users : search for everything related to this user
+#     return  only rows where reading or read or will_read    are not null
+
+
+
+def statistics (username) :
+    pass
+#     in users : search for everything related to this user WHERE read is not null + inner join with books ON book_id
+        
+#     now , for books_you_read :  group the results by book id ? 
+#     use join in similar way to find all other statistics,
+#     find a way to display results
+        
+    
+    
+ 
+  
+
+# sql('select * from books')
+# search_by_author("Suzanne collins")
+# recently_added ('Science fiction')
+# most_read_books('Science fiction')
+# # most_favorite_books ()
+# most_read_genres   ()
+print (most_read_authors ())
